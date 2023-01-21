3def82977e141bcffaa71adfda20f7679f930081
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-20 19:32:39 -0800

    handleChange function updated e.target.value for name and prompt input, handelSurpriseMe function updated to getRandomPrompt and render on form, AI image preview div, generate and share buttons created

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,20 +17,25 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {
+
+  }
+
   const handleSubmit = () => {
 
   }
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
   const handleSurpriseMe = () => {
-    
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
   }
 
   return (
     <section className='max-w-7xl mx-auto'>
-        {/* Heading and Subtitle */}
+    {/* Heading and Subtitle */}
       <div>
         <h1 className='font-extrabold text-[#222328] text-[32px]'>
           Create AI Images 
@@ -39,7 +44,7 @@ const CreatePost = () => {
           Explore to test out your very own DALL-E OpenAI generated images! Share your creativity with the community!
         </p>
       </div>
-
+    {/* Name and Prompt FormFields */}
       <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
           <FormField 
@@ -60,6 +65,50 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+        {/* AI Image */}
+          <div className='relative bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-100 p-3 h-100 flex justify-center items-center'>
+            {form.photo ? (
+              <img 
+                src={form.photo}
+                alt={form.prompt}
+                className='w-full h-full object-contain'
+              />
+            ) : (
+              <img 
+                src={preview}
+                alt='preview'
+                className='w-9/12 h-9/12 object-contain opacity-40'
+              />
+            )}
+            
+            {generatingImg && (
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+      {/* Submit Button */}
+        <div className='mt-5 flex gap-5'>
+          <button
+            type='button'
+            onClick={generateImage}
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+      {/* Share Button */}
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>
+            Feel free to share your AI generated image with the community! 
+          </p>
+          <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {loading ? 'Sharing...' : 'Share to Community!'}
+          </button>
         </div>
       </form>
     </section>
