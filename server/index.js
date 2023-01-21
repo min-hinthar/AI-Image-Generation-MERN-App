0d92c6af5eaffa1baba97e8814371514de8ef0ef
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-20 20:23:34 -0800

    routes folder created for postRoutes and dalleRoutes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 // environment variables
 dotenv.config();
@@ -11,6 +13,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit:  '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async(req, res) => {
     res.send('Hello from DALL-E!');
