1c5d9635c163901460f0e07d34c775e226cb16f5
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-20 20:33:21 -0800

    openAi route updated with API key from OpenAI stored inside .env, route functional and running

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
 import Post from '../mongodb/models/post.js';
@@ -7,3 +7,6 @@ import Post from '../mongodb/models/post.js';
 dotenv.config();
 
 const router = express.Router();
+
+
+export default router;
