d93cd93c8f51520d0d146ee0fa4f85b402982899
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-01-20 18:56:18 -0800

    FormField component updated with params labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe, input text styling updated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,8 +6,63 @@ import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+    
+  }
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+        {/* Heading and Subtitle */}
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create AI Images 
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
+          Explore to test out your very own DALL-E OpenAI generated images! Share your creativity with the community!
+        </p>
+      </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField 
+            labelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Aung San Suu Kyi'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField 
+            labelName='Prompt'
+            type='text'
+            name='prompt'
+            placeholder='a surrealist dream-like oil painting by Salvador Dalí of a cat playing chess with Van Gogh in a starry night'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
